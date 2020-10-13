1ef97a052c2ab3f7670764fc2a581a1882166c26
sebsmad <sebsmad@gmail.com>
2020-10-13 05:03:12 -0400

    course update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,7 +64,7 @@ module.exports = {
           '/modules/week3/',
           '/modules/week4/',
           '/modules/week5/',
-          // '/modules/week6/',
+          '/modules/week6/',
           // '/modules/week7/',
           // '/modules/break/',
           // '/modules/week8/',
